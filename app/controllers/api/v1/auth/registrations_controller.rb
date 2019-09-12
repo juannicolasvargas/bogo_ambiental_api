a94f5586d2b6a64fd604c8a8d650a8e74af7592a
Nicolas Vargas Quiroga <jelboy33@gmail.com>
2019-09-11 20:54:38 -0500

    create module for login with devise token auth

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+        private
+
+        def sign_up_params
+          params.permit(:email, :password, :password_confirmation, :name, :last_name)
+        end
+      end
+    end
+  end
+end
