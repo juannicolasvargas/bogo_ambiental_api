5a3b07abf916263256c76093765ee668020f4c85
Nicolas Vargas Quiroga <jelboy33@gmail.com>
2019-09-26 13:06:42 -0500

    require fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,5 @@
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -6,4 +8,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory = ENV['S3_BUCKET_NAME']
+  config.storage = :fog
 end
