a684ec216a38d2f01ca57009f383903cff454678
Nicolas Vargas Quiroga <nicolas.vargas1012@gmail.com>
2019-10-01 13:11:07 -0500

    Revert "add uploader image in controller avatar"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  include RenderHelper
-
-  def current_user
-    current_api_v1_user
-  end
-
-  def authenticate_user!
-    authenticate_api_v1_user!
-  end
 end
