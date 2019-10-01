bb9a4a8b7fc1bc58e2259e882ace9bb694eff1fa
Nicolas Vargas Quiroga <jelboy33@gmail.com>
2019-10-01 16:21:37 -0500

    modify render sessions controller

---
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -0,0 +1,13 @@
+
+module Api
+  module V1
+    module Auth
+      class SessionsController < DeviseTokenAuth::SessionsController
+
+        def render_create_success
+          render json: current_user
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: 'api/v1/auth/registrations',
+        sessions: 'api/v1/auth/sessions'
       }
       resources :avatar_images, only: [:create]
     end
