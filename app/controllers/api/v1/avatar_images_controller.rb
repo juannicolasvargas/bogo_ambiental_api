ae41fbdb3f9b115550271b660886e62c2170e953
Nicolas Vargas Quiroga <jelboy33@gmail.com>
2019-10-19 03:38:40 -0500

    update upload files

--- app/controllers/api/v1/avatar_images_controller.rb
+++ app/controllers/api/v1/avatar_images_controller.rb
@@ -4,25 +4,11 @@ module Api
       before_action :authenticate_user!
 
       def create
-        current_user.avatar_image = avatar_image_user
-        avatar_image_user.close
+        current_user.avatar_image = params[:avatar_image]
         if current_user.save!
-          render_success(current_user)
+          render json:  current_user.avatar_image.url
         end
       end
-
-      private
-
-      def avatar_image_user
-        @avatar_image_user ||= begin
-          image_data = Base64.decode64(params[:avatar_image])
-          temp_file = Tempfile.new([current_user.name,'.jpg'])
-          temp_file.binmode
-          temp_file.write(image_data)
-          temp_file
-        end
-      end
-
     end
   end
 end
