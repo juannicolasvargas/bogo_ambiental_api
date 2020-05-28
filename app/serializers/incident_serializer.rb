4d05d9974e5c170e48c035510be672b5cfc19ed4
Nicolas Vargas <nicolas.vargas1012@gmail.com>
2020-05-28 16:41:59 -0500

    add created_at incident

--- app/serializers/incident_serializer.rb
+++ app/serializers/incident_serializer.rb
@@ -1,5 +1,8 @@
 class IncidentSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :user_id
+  attribute :created_at do
+    object.created_at.to_date.to_s
+  end
   attribute :image do
     object.image.present? ? object.image.url : ''
   end
